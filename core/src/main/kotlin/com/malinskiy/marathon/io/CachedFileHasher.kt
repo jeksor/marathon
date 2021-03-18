85e8a1cc949481141bf0446e00d844f46a1ca720
Nickolay Chameyev <nickolay.chameyev@corp.badoo.com>
2021-03-18 19:20:43 +0000

    Syncrhonize file hasher

--- core/src/main/kotlin/com/malinskiy/marathon/io/CachedFileHasher.kt
+++ core/src/main/kotlin/com/malinskiy/marathon/io/CachedFileHasher.kt
@@ -2,18 +2,19 @@ package com.malinskiy.marathon.io
 
 import org.apache.commons.collections4.map.LRUMap
 import java.io.File
+import java.util.*
 
 class CachedFileHasher(
     private val delegate: FileHasher,
     cacheCapacity: Int = DEFAULT_CACHE_CAPACITY
 ) : FileHasher {
 
-    private val cache = LRUMap<File, String>(cacheCapacity)
+    private val cache = Collections.synchronizedMap(LRUMap<File, String>(cacheCapacity))
 
     override suspend fun getHash(file: File): String =
         cache.getOrPut(file) { delegate.getHash(file) }
 
     private companion object {
-        private const val DEFAULT_CACHE_CAPACITY = 128
+        private const val DEFAULT_CACHE_CAPACITY = 512
     }
 }
